430beab93e388d6cb4e23f0fc94a37399ccd2d23
mkvasconcelos <mateuskvasconcelos@gmail.com>
2022-10-19 20:10:31 -0300

    API CHAT UOL Enter Room and stay online requests are working

---
+++ js/main.js
@@ -0,0 +1,26 @@
+let trigger = 400;
+// while(trigger !== 200){
+const participant = prompt("Qual seu nome de usuário?");
+let participant_object = {'name':participant};
+const enter_room = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants',participant_object);
+enter_room.then(function success(response){
+    console.log(response.status);
+    return response.status;
+});
+enter_room.catch(function failure(response){
+    console.log(response.status);
+    return response.status;
+});
+// }
+
+setInterval((function (){
+    const stay_online = axios.post('https://mock-api.driven.com.br/api/v6/uol/status',participant_object);
+    stay_online.then(function success(response){
+        console.log(response.status);
+        return response.status;
+    });
+    stay_online.catch(function failure(response){
+        console.log(response.status);
+        return response.status;
+    });
+}),5000);
