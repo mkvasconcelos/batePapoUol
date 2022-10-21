8a3dd64da978bbeddd308b83ba14f6817f9040df
mkvasconcelos <mateuskvasconcelos@gmail.com>
2022-10-20 21:41:38 -0300

    Search messages working

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+const main = document.querySelector("main");
+let ul = main.children[0];
+let date = new Date().toJSON().split("T")[1].split(".")[0];
+let date_hour = Number(date.split(":")[0])+12;
+let date_minute = Number(date.split(":")[1]);
+
 let trigger = 400;
 // while(trigger !== 200){
 const participant = prompt("Qual seu nome de usuário?");
@@ -16,21 +22,54 @@ enter_room.catch(function failure(response){
 setInterval((function (){
     const stay_online = axios.post('https://mock-api.driven.com.br/api/v6/uol/status',participant_object);
     stay_online.then(function success(response){
-        console.log(response.status);
+        // console.log(response.status);
         return response.status;
     });
     stay_online.catch(function failure(response){
-        console.log(response.status);
+        // console.log(response.status);
         return response.status;
     });
 }),5000);
 
 let search_messages = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
 search_messages.then(function success(response){
-    console.log(response.data);
-    return response.status;
+    // console.log(response.data);
+    for(let i = 0; i < response.data.length; i++){
+        if(search_name(response.data[i],participant)){
+            add_messages(response.data[i]);
+        }
+    }
+    return response.data;
 });
 search_messages.catch(function failure(response){
-    console.log(response);
+    // console.log(response);
     return response.status;
 });
+
+function add_messages(object){
+    if (object.type === 'status'){
+        ul.innerHTML += 
+        `<li>
+            <p>(${object.time}) <span>${object.from}</span> (${object.text})</p> 
+        </li>`
+    } else if (object.type === 'message'){
+        `<li>
+            <p>(${object.time}) <span>${object.from}</span> para <span>${object.to}</span>: (${object.text})</p> 
+        </li>`
+    } else{
+        `<li>
+            <p>(${object.time}) <span>${object.from}</span> reservadamente para <span>${object.to}</span>: (${object.text})</p> 
+        </li>`  
+    }
+}
+
+function search_name(object, name){
+    const hour = Number(object.time.split(":")[0]);
+    const minute = Number(object.time.split(":")[1]);
+    if (hour >= date_hour){
+        if (minute >= (date_minute)){
+            if(object.from === name || object.to === 'Todos' || object.to === name){
+                return true;
+            }}} 
+    return false;
+}
