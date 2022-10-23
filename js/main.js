636e93b896ba27e0779135a59848c1cd3bd00980
mkvasconcelos <mateuskvasconcelos@gmail.com>
2022-10-23 11:51:18 -0300

    Almost done. MVP is working, but the participants are not ticking and the homepage arent done

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ const aside_right = document.querySelector('.right_side');
 const aside_left = document.querySelector('.left_side');
 let ul = main.children[0];
 let participant = "";
+let list_message = [{"id": "", "value": "Todos"},{"id":aside_right.children[3].children[0].children[0].children[1], "value": "message"}] 
 
 start_chat();
 
@@ -13,6 +14,7 @@ function start_chat(){
     const enter_room = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants',{'name':participant});
     enter_room.then(function success(response){
         add_participant();
+        list_message[0].id = document.querySelector('.todos');
         return response.status;
     });
     enter_room.catch(function failure(response){
@@ -68,19 +70,19 @@ function add_messages(object){
         `<li class=${object.type}>
             <p><span class="time">(${object.time})</span> <span>${object.from}</span> ${object.text}</p> 
         </li>`
-        
+        ul.children[ul.children.length-1].scrollIntoView(true);
     } else if (object.type === 'message'){
         ul.innerHTML +=
         `<li>
             <p><span class="time">(${object.time})</span> <span>${object.from}</span> para <span>${object.to}</span>: ${object.text}</p> 
         </li>`
-        
+        ul.children[ul.children.length-1].scrollIntoView(true);
     } else{
         ul.innerHTML +=
         `<li class=${object.type}>
             <p><span class="time">(${object.time})</span> <span>${object.from}</span> reservadamente para <span>${object.to}</span>: ${object.text}</p> 
         </li>`  
-        
+        ul.children[ul.children.length-1].scrollIntoView(true);
     }
 }
 
@@ -92,8 +94,8 @@ function search_name(object, name){
 }
 
 function send_messages(){
-    const message = footer.children[0].value;
-    let message_object = {'from': participant, 'to': 'Todos', 'text': message, 'type': 'message'};
+    const message = footer.children[0].children[0].value;
+    let message_object = {'from': participant, 'to': list_message[0].value, 'text': message, 'type': list_message[1].value};
     const message_api = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages",message_object);
     message_api.then(function success(response){
         clear_message();
@@ -101,12 +103,13 @@ function send_messages(){
     });
     message_api.catch(function failure(response){
         console.log("falha", response);
+        window.location.reload();
         return response;
     });
 }
 
 function clear_message(){
-    return footer.children[0].value = "";
+    return footer.children[0].children[0].value = "";
 }
 
 function participant_online(){
@@ -117,17 +120,23 @@ function participant_online(){
 function add_participant(){
     const ul_aside = aside_right.children[1].children[0];
     ul_aside.innerHTML = 
-    `<li>
-        <ion-icon name="people"></ion-icon>
-        <h2>Todos</h2>
+    `<li data-identifier="participant">
+        <div onclick="select_participant(this)">
+            <ion-icon name="people"></ion-icon>
+            <h2>Todos</h2>
+        </div>
+        <ion-icon class="todos" id="check" name="checkmark-outline"></ion-icon>
     </li>`;
     const inside_room = axios.get('https://mock-api.driven.com.br/api/v6/uol/participants');
-    inside_room.then(function success(response){
+    inside_room.then(function success(response){ 
         for(let i = 0; i < response.data.length; i++){
             ul_aside.innerHTML += 
-            `<li>
-                <ion-icon name="person-circle-outline"></ion-icon>
-                <h2>${response.data[i].name}</h2>
+            `<li data-identifier="participant">
+                <div onclick="select_participant(this)">
+                    <ion-icon name="person-circle-outline"></ion-icon>
+                    <h2>${response.data[i].name}</h2>
+                </div>
+                <ion-icon id="check" name="checkmark-outline"></ion-icon>
             </li>`; 
         }
         return response.status;
@@ -138,7 +147,33 @@ function add_participant(){
     });
 }
 
-const input = footer.children[0];
+function select_participant(type){
+    const div_chosen = type.parentNode.parentNode.parentNode;
+    const icon = type.parentNode.children[1];
+    if (div_chosen === aside_right.children[1]){
+        console.log(list_message[0].id);
+        console.log(list_message[0].id.parentNode);
+        list_message[0].id.classList.toggle("check_on");
+        icon.classList.toggle("check_on");
+        list_message[0].id = icon;
+        list_message[0].value = type.children[1].innerText;
+        footer.children[0].children[1].children[0].innerText = type.children[1].innerText;;
+    } else {
+        list_message[1].id.classList.toggle("check_off");
+        icon.classList.toggle("check_off");
+        list_message[1].id = icon;
+        if(type.children[1].innerText === "Público"){
+            list_message[1].value = "message";
+            footer.children[0].children[1].children[1].innerText = "público";
+        } else{
+            list_message[1].value = "private_message";
+            footer.children[0].children[1].children[1].innerText = "reservadamente";
+        }
+        console.log(list_message);
+    }
+}
+
+const input = footer.children[0].children[0];
 input.addEventListener("keypress", function(event) {
   if (event.key === "Enter") {
     event.preventDefault();
